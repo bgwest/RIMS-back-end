ba79b3e44d8c5403726208a0fdcbea72051dc035
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2019-02-24 08:30:00 -0800

    begin setting up new middleware

---
+++ src/lib/recoveryAnswerAuth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const HttpError = require('http-errors');
+const Account = require('../model/account');
+
+// setup exports
+module.exports = {};
+
+const recoveryAnswerAuth = (request, response, next) => {
+
+};
+
+module.exports.recoveryAnswerAuth = recoveryAnswerAuth;
